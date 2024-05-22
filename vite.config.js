0523320760529903516f09d6797fb9bab32b59a1
Charugulla-Sai <satyasaicharugulla.3@gmail.com>
2024-05-22 17:06:27 +0530

    update vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://example.com/realApi', // Replace with your API server URL
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   optimizeDeps: {
-    exclude: ['js-big-decimal']
-  }
-})
+    exclude: ['js-big-decimal'],
+  },
+});
